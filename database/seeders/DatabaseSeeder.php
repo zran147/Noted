9b5084ad14c3764a83e762060ce7d37b6ee8bb98
salmadanu <salma.danuningrat@gmail.com>
2023-06-01 16:17:18 +0700

    Update Database Seeder
    Update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,70 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // User::create([
-        //     'namalengkap' => 'Salma',
-        //     'username' => 'salma123',
-        //     'email' => 'salma@gmail.com',
-        //     'password' => bcrypt('12345')
-
-        // ]);
-
-        // Note::create([
-        //     'judul_note' => 'Strukdat Pekan 10',
-        //     'kategorinotes_id' => 1,
-        //     'slug' => 'strukdat-pekan-10',
-        //     'excerpt_note' => 'Test',
-        //     'kategori_note' => 'College',
-        //     'isi_note' => 'Catatan struktur data disimpan disini'
-        // ]);
-        
-        
-        // Note::create([
-        //     'judul_note' => 'Pemrog Pekan 10',
-        //     'kategorinotes_id' => 1,
-        //     'slug' => 'pemrog-pekan-10',
-        //     'excerpt_note' => 'Java Inheritance',
-        //     'kategori_note' => 'College',
-        //     'isi_note' => 'Catatan pemrog disimpan disini'
-        // ]);
-
-        // Note::create([
-        //     'judul_note' => 'Notulensi Rapat',
-        //     'kategorinotes_id' => 2,
-        //     'slug' => 'notulensi',
-        //     'kategori_note' => 'Personal',
-        //     'isi_note' => 'Notul rapat kemarin ditulis disini'
-        // ]);
-
-        //  Note::create([
-        //     'judul_note' => 'Strukdat Pekan 10',
-        //     'kategorinotes_id' => 1,
-        //     'slug' => 'strukdat-pekan-10',
-        //     'kategori_note' => 'College',
-        //     'isi_note' => 'Catatan struktur data disimpan disini',
-        //     'user_id' => 13
-        // ]);
-
-        // Note::create([
-        //     'judul_note' => 'GKV Tugas Akhir',
-        //     'kategorinotes_id' => 1,
-        //     'kategori_note' => 'Academics',
-        //     'slug' => 'gkv-tugas-akhir',
-        //     'isi_note' => 'Catatan GKV disimpan disini',
-        //     'user_id' => 13
-        // ]);
-
-
-        // Note::create([
-        //     'judul_note' => 'Notulensi Rapim',
-        //     'kategorinotes_id' => 2,
-        //     'slug' => 'notulensi-rapim',
-        //     'isi_note' => 'Notul rapim',
-        //     'user_id' => 13
-        // ]);
-
+       
+        //Step 1: Bikin Kategorinotes
         Kategorinotes::create([
             'nama' => 'Akademik',
             'slug' => 'akademik',   
@@ -95,80 +33,41 @@ class DatabaseSeeder extends Seeder
             'slug' => 'organisasi',   
         ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        //Step 2: Bikin Kategoritransaksi
+        Kategoritransaksi::create([
+            'nama' => 'Orang tua',
+            'slug' => 'orang-tua',   
+        ]);
 
-        // Kategoripemasukan::create([
-        //     'nama' => 'Bisnis',
-        //     'slug' => 'bisnis',   
-        // ]);
+        Kategoritransaksi::create([
+            'nama' => 'Beasiswa',
+            'slug' => 'beasiswa',   
+        ]);
 
-        // Kategoripemasukan::create([
-        //     'nama' => 'Beasiswa',
-        //     'slug' => 'Beasiswa',   
-        // ]);
+        Kategoritransaksi::create([
+            'nama' => 'Bisnis',
+            'slug' => 'bisnis',   
+        ]);
 
-        // Kategoripemasukan::create([
-        //     'nama' => 'Orang tua',
-        //     'slug' => 'orang-tua',   
-        // ]);
+        Kategoritransaksi::create([
+            'nama' => 'Makan dan Minum',
+            'slug' => 'makan-dan-minum',   
+        ]);
 
-        // Pemasukan::create([
-        //     'userspemasukan_id' => 13,
-        //     'judul_pemasukan' => 'Gaji bulan Mei',
-        //     'kategoripemasukan_id' => 2,
-        //     'pemasukan_nominal' => 50000,
-        // ]);
+        Kategoritransaksi::create([
+            'nama' => 'Akomodasi',
+            'slug' => 'akomodasi',   
+        ]);
 
+        Kategoritransaksi::create([
+            'nama' => 'Pribadi',
+            'slug' => 'pribadi',   
+        ]);
 
-        //STEP 1
-        // Kategoritransaksi::create([
-        //     'nama' => 'Orang tua',
-        //     'slug' => 'orang-tua',   
-        // ]);
-
-        // Kategoritransaksi::create([
-        //     'nama' => 'Beasiswa',
-        //     'slug' => 'beasiswa',   
-        // ]);
-
-        // Kategoritransaksi::create([
-        //     'nama' => 'Bisnis',
-        //     'slug' => 'bisnis',   
-        // ]);
-
-        // Kategoritransaksi::create([
-        //     'nama' => 'Makan dan Minum',
-        //     'slug' => 'makan-dan-minum',   
-        // ]);
-
-        // Kategoritransaksi::create([
-        //     'nama' => 'Akomodasi',
-        //     'slug' => 'akomodasi',   
-        // ]);
-
-        // Kategoritransaksi::create([
-        //     'nama' => 'Pribadi',
-        //     'slug' => 'pribadi',   
-        // ]);
-
-        // Kategoritransaksi::create([
-        //     'nama' => 'Kuliah',
-        //     'slug' => 'kuliah',   
-        // ]);
-
-
-
-
-        // Transaksi::create([
-        //     'userstransaksi_id' => 13,
-        //     'judul_transaksi' => 'Hasil Danus',
-        //     'kategoritransaksi_id' => 1,
-        //     'jenis_transaksi' => 'pemasukan',
-        //     'nominal_transaksi' => 100000,
-        // ]);
+        Kategoritransaksi::create([
+            'nama' => 'Kuliah',
+            'slug' => 'kuliah',   
+        ]);
     }
 }
 
