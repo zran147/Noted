050ba48f5046cd2f92962818cbdaae52e5126f09
salmadanu <salma.danuningrat@gmail.com>
2023-06-04 23:52:23 +0700

    Grafik Moneybox

--- resources/views/moneybox/index.blade.php
+++ resources/views/moneybox/index.blade.php
@@ -5,7 +5,7 @@
 @extends('layouts.main')
 
 @section('before')
-    @include('partials.navbar', ['title' => 'Home'])
+    @include('partials.navbar', ['title' => 'MoneyBox'])
     @if(session()->has('success'))
         <div class="alert alert-success alert-dismissible fade show" role="alert">
             {{ session('success') }}
@@ -17,59 +17,58 @@
             {{ session('loginError') }}
         </div>
     @endif
+
+    <!-- Include the Google Chart API library -->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+        google.charts.load('current', { 'packages': ['corechart'] });
+        google.charts.setOnLoadCallback(drawCharts);
+
+        function drawCharts() {
+            @foreach ($moneyboxes as $moneybox)
+                drawChart({{ $moneybox->id }}, {{ $moneybox->nominal_moneybox }}, {{ $moneybox->target_moneybox }});
+            @endforeach
+        }
+
+        function drawChart(moneyboxId, currentAmount, targetAmount) {
+            var data = google.visualization.arrayToDataTable([
+                ['Label', 'Value'],
+                ['Current', currentAmount],
+                ['Remaining', targetAmount - currentAmount]
+            ]);
+
+            var options = {
+                pieHole: 0.6,
+                pieSliceTextStyle: {
+                    color: 'black',
+                },
+                slices: {
+                    0: { color: '#FFA559' },
+                    1: { color: '#454545' }
+                },
+                legend: 'none',
+                backgroundColor: 'none',
+                chartArea: {
+                    left: 20,
+                    top: 20,
+                    width: '100%',
+                    height: '100%'
+                },
+                fontSize: 12
+            };
+
+            var chart = new google.visualization.PieChart(document.getElementById('moneyboxChart' + moneyboxId));
+            chart.draw(data, options);
+        }
+    </script>
 @endsection
 
 @section('container')
     <div class="row mt-5">
         <div class="col-lg-6">
-            @section('scripts')
-            <script type="text/javascript">
-                function loadCharts() {
-                    google.charts.load('current', {'packages':['corechart']});
-                    google.charts.setOnLoadCallback(drawCharts);
-                }
-        
-                function drawCharts() {
-                    @foreach ($moneyboxes as $moneybox)
-                        drawChart({{ $moneybox->id }}, {{ $moneybox->nominal_moneybox }}, {{ $moneybox->target_moneybox }});
-                    @endforeach
-                }
-        
-                function drawChart(moneyboxId, currentAmount, targetAmount) {
-                    var data = google.visualization.arrayToDataTable([
-                        ['Label', 'Value'],
-                        ['Current', currentAmount],
-                        ['Remaining', targetAmount - currentAmount]
-                    ]);
-        
-                    var options = {
-                        pieHole: 0.6,
-                        pieSliceTextStyle: {
-                            color: 'black',
-                        },
-                        slices: {
-                            0: { color: '#FFA559' },
-                            1: { color: '#454545' }
-                        },
-                        legend: 'none',
-                        backgroundColor: 'none',
-                        chartArea: {
-                            left: 20,
-                            top: 20,
-                            width: '100%',
-                            height: '100%'
-                        },
-                        fontSize: 12
-                    };
-        
-                    var chart = new google.visualization.PieChart(document.getElementById('moneyboxChart' + moneyboxId));
-                    chart.draw(data, options);
-                }
-        
-                loadCharts();
-            </script>
-        @endsection
-        
+            @foreach ($moneyboxes as $moneybox)
+                <div id="moneyboxChart{{ $moneybox->id }}" style="width: 100%; height: 300px;"></div>
+            @endforeach
         </div>
 
         <div class="col-lg-6">
@@ -83,20 +82,23 @@
                         </form>
                         <div class="row mb-2 mt-3">
                             <div class="col-6">{{ $moneybox->judul_moneybox }}</div>
-                            <div class="col-6 text-end"> {{ number_format($moneybox->target_moneybox, 2, ',', '.')  }}</div>
+                            <div class="col-6 text-end">{{ number_format($moneybox->target_moneybox, 2, ',', '.') }}</div>
                         </div>
                         <div class="row mb-2">
-                            <div class="col-6"> {{ number_format($moneybox->nominal_moneybox, 2, ',', '.') }} </div>
+                            <div class="col-6">{{ number_format($moneybox->nominal_moneybox, 2, ',', '.') }}</div>
                             <div class="col-6 text-end">{{ $moneybox->updated_at->format('j F') }}</div>
                         </div>
                         @php
-                        $remainingDays = \Carbon\Carbon::parse($moneybox->tanggal_selesai)->diffInDays(\Carbon\Carbon::now());
+                            $remainingDays = \Carbon\Carbon::parse($moneybox->tanggal_selesai)->diffInDays(\Carbon\Carbon::now());
                         @endphp
                         <div class="row mb-2">
                             <div class="col-6">{{ $remainingDays }} days left</div>
                         </div>
-                        <div class="progress mb-3" role="progressbar" aria-label="Basic example" aria-valuenow="{{ $moneybox->nominal_moneybox }}" aria-valuemin="0" aria-valuemax="{{ $moneybox->target_moneybox }}">
-                            <div class="progress-bar" style="width: {{ ($moneybox->nominal_moneybox / $moneybox->target_moneybox) * 100 }}%"></div>
+                        <div class="progress mb-3" role="progressbar" aria-label="Basic example"
+                             aria-valuenow="{{ $moneybox->nominal_moneybox }}" aria-valuemin="0"
+                             aria-valuemax="{{ $moneybox->target_moneybox }}">
+                            <div class="progress-bar"
+                                 style="width: {{ ($moneybox->nominal_moneybox / $moneybox->target_moneybox) * 100 }}%"></div>
                         </div>
                         <a href="{{ route('moneybox.addmoney', $moneybox->id) }}" class="mb-3 btn btn-info">Add Money</a>
                     </div>
@@ -106,4 +108,3 @@
         </div>
     </div>
 @endsection
-
