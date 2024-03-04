aabd18cb159d0c9419a4cd2b02a61b8629ef577a
zran147 <muhammadzahran@apps.ipb.ac.id>
2024-03-04 11:28:51 +0700

    fixed css and routing

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -7,32 +7,32 @@
 
 @section('container')
 <div class="row justify-content-center">
-    <div class="col-10 col-sm-8 col-md-8 col-lg-7">
-        <main class="form-signin w-100 m-auto text-center">
-            <h1 class="display-5 mb-5 fw-normal" id="noted">Noted.</h1>
-            <form class="text-start poppins brown" action="{{ route('login') }}" method="post">
-              <div class="p-1">Username</div>
-              @csrf
-              <div class="form-floating mb-2">
-                <input type="username" name="username" class="form-control form-control-md @error('username') is-invalid @enderror" id="floatingInput" autofocus required value="{{ old('username') }}">
-                @error('username')
-                <div class="invalid-feedback">
-                  {{ $message }}
-                </div>
-                @enderror
+  <div class="col-10 col-sm-8 col-md-8 col-lg-7">
+      <main class="form-signin w-100 m-auto text-center">
+          <h1 class="display-5 mb-5 fw-normal" id="noted">Noted.</h1>
+          <form class="text-start poppins brown" action="{{ route('login') }}" method="post">
+            <div class="p-1">Username</div>
+            @csrf
+            <div class="form-floating mb-2">
+              <input type="username" name="username" class="form-control form-control-md @error('username') is-invalid @enderror" id="floatingInput" autofocus required value="{{ old('username') }}">
+              @error('username')
+              <div class="invalid-feedback">
+                {{ $message }}
               </div>
-              <div class="p-1">Password</div>
-              <div class="form-floating mb-5">
-                <input type="password" name="password" class="form-control form-control-md"  id="password" required>
-              </div>
-              <div class="p-1 d-flex justify-content-between">
-                <small>Not Registered?</small>
-                <small><a href="/register">Register</a></small>
-              </div>
+              @enderror
+            </div>
+            <div class="p-1">Password</div>
+            <div class="form-floating mb-5">
+              <input type="password" name="password" class="form-control form-control-md"  id="password" required>
+            </div>
+            <div class="p-1 d-flex justify-content-between">
+              <small>Not Registered?</small>
+              <small><a href="/register">Register</a></small>
+            </div>
 
-              <button class="w-100 btn btn-md btn-primary" type="login">Login</button>
-            </form>
-        </main>
-    </div>
+            <button class="w-100 btn btn-md btn-primary" type="login">Login</button>
+          </form>
+      </main>
+  </div>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,13 @@
 <?php
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\HomeNotesController;
 use App\Http\Controllers\MoneyboxController;
-use App\Http\Controllers\transaksiController;
+use App\Http\Controllers\TransaksiController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\NoteController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
 use App\Models\Kategorinotes;
-use App\Models\Kategoritransaksi;
 use Illuminate\Support\Facades\Route;
-use App\Models\Note;
 
 
 /*
